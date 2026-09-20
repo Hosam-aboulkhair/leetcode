6125993379da8e98131dacfeb338ecf6945a7173
AbouLkhair <husam.tariq@outlook.com>
2026-09-21 00:27:54 +0300

    Update 20-Valid Parentheses.java

--- java/20-Valid Parentheses.java
+++ java/20-Valid Parentheses.java
@@ -44,3 +44,22 @@ public class Solution {
         return stack.isEmpty();
     }
 }
+-----------------------------------------
+    class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> pairs = Map.of(')', '(', '}', '{', ']', '[');
+        for(char c:s.toCharArray()){
+            if(pairs.containsKey(c)){
+            if(!stack.isEmpty() && stack.peek() == pairs.get(c)){
+                stack.pop();
+            }else{
+                return false;
+                }
+            }else {
+            stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
